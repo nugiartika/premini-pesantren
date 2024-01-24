1a4262e16672adfce133af83e69a13d9f2f9f3c7
parill <157394749+parill@users.noreply.github.com>
2024-01-24 16:16:18 +0700

    LARAVEL

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 </head>
 <body>
     <div id="app">
+        @if(!request()->is('login') && !request()->is('register'))
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -71,7 +72,7 @@
                 </div>
             </div>
         </nav>
-
+        @endif
         <main class="py-4">
             @yield('content')
         </main>
